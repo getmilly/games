6078e4f764b6536ece5d013537783ee5ba26d610
Guilherme Firmiano Gonçalves <firmiano@MacBook-Pro-de-Guilherme.local>
2019-04-22 23:40:23 -0300

    adding swagger

--- api/main.go
+++ api/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/getmilly/games/api/controllers"
 	"github.com/getmilly/grok/api"
-	gnats "github.com/getmilly/grok/nats"
 	"github.com/sarulabs/di"
 )
 
@@ -16,25 +15,7 @@ func main() {
 		Name:  "app-controller",
 		Scope: di.App,
 		Build: func(c di.Container) (interface{}, error) {
-			producer := c.Get("nats-producer").(*gnats.Producer)
-			return controllers.NewAppController(producer), nil
-		},
-	})
-
-	server.AddDependency(di.Def{
-		Name:  "nats-producer",
-		Scope: di.App,
-		Build: func(c di.Container) (interface{}, error) {
-			conn := c.Get("nats-conn").(*nats.EncodedConn)
-			return gnats.NewProducer(conn), nil
-		},
-	})
-
-	server.AddDependency(di.Def{
-		Name:  "nats-conn",
-		Scope: di.App,
-		Build: func(c di.Container) (interface{}, error) {
-			return gnats.Connect(nats.DefaultURL, "")
+			return controllers.NewAppController(), nil
 		},
 	})
 
